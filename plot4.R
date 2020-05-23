53fe47b6d11b6d0272334278c9caf4fc6fd4a01f
Garrett McLean <gmclean@gmail.com>
2020-05-23 12:32:39 -0700

    added code to download and extract raw data

--- plot4.R
+++ plot4.R
@@ -1,11 +1,17 @@
 library(dplyr)
 
+#Get the data
+download.file(url = 'https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip', destfile = 'data.zip')
+unzip('data.zip')
+
+#Load the data
 x <- read.table('household_power_consumption.txt', sep = ';', header = TRUE, 
                 colClasses = c(rep('character', 2), rep('numeric', 7)), na.strings = '?') %>% 
     filter(grepl("^[1-2]{1}/2/2007$", Date)) %>%
     mutate(Datetime = as.POSIXct(paste(Date, Time), format = '%d/%m/%Y %H:%M:%S'))  %>% 
     select(-Date, -Time) 
 
+#Make the plot
 png('plot4.png', width = 480, height = 480)
 
 par(mfrow = c(2,2))
